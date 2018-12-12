ea48bb00d3d5f7ef042e74134df001d5cdb9ef5a
Lucas Suarez <lucas.suarez.utepsa@gmail.com>
2018-12-12 19:17:57 -0400

    Cambio de nombre de la carpeta build/docs

---
+++ docs/precache-manifest.238f9584ae48bca61497a6ce2da9da22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f248d7483f45514b48d",
+    "url": "/routingreact/static/js/runtime~main.2f248d74.js"
+  },
+  {
+    "revision": "3c1eaecfbdfb598b9323",
+    "url": "/routingreact/static/js/main.3c1eaecf.chunk.js"
+  },
+  {
+    "revision": "9d321e85bc7b2dddf1b1",
+    "url": "/routingreact/static/js/1.9d321e85.chunk.js"
+  },
+  {
+    "revision": "3c1eaecfbdfb598b9323",
+    "url": "/routingreact/static/css/main.a54eb588.chunk.css"
+  },
+  {
+    "revision": "e424a4d1178b29ef293ebe3a62ad5178",
+    "url": "/routingreact/index.html"
+  }
+];
