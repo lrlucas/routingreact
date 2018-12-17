fc89c47c66fc0cce2b209643b5789694503456dc
Lucas Suarez <lucas.suarez.utepsa@gmail.com>
2018-12-17 15:21:33 -0400

    Modificada la ruta de la imagen en el componente de nosotros

--- src/components/About/Nosotros.js
+++ src/components/About/Nosotros.js
@@ -11,7 +11,7 @@ export default class Nosotros extends Component {
         return (
             <div className="contenedor-nosotros">
                 <div className="imagen">
-                    <img src="/img/camisa_1.png" alt="iamgen nosotros"/>
+                    <img src="./img/camisa_1.png" alt="imagen nosotros"/>
                 </div>
                 <div className="contenido">
                     <h2>Sobre nosotros</h2>
